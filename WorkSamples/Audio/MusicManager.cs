501e671a4758c3b9bc88f6217b9ecd993d9681b3
Omar Alzayed <pm@moving-dimensions.com>
2023-09-20 09:27:43 -0700

    Some cleaning up

--- WorkSamples/Audio/MusicManager.cs
+++ WorkSamples/Audio/MusicManager.cs
@@ -2,8 +2,7 @@
 {
     public class MusicManager : Singleton<MusicManager>
     {
-        [System.Serializable]
-        public class QueuedSheet
+        protected class QueuedSheet
         {
             public AudioSource source;
             public MusicSheet sheet;
@@ -11,51 +10,77 @@
             public float stopTimer;
             public bool markedForDequeue;
         }
-        public struct ControlledVolume
+        protected struct ControlledVolume
         {
             public float volume;
             public GameObject source;
         }
-
+        /// <summary>
+        /// Global audio sources that have been registered in the system
+        /// </summary>
         private static List<AudioSource> sources;
+        /// <summary>
+        /// Current volume manual overrides. If empty, default volume is used
+        /// </summary>
         protected List<ControlledVolume> controlledVolumes = new List<ControlledVolume>();
-        public static float controlledVolume
+        /// <summary>
+        /// Sheets played
+        /// </summary>
+        protected Sta<QueuedSheet> queue = new List<QueuedSheet>();
+        /// <summary>
+        /// Current controlled volume that accounts for overrides
+        /// </summary>
+        public static float ControlledVolume
         {
             get
             {
-                if (instance != null && instance.controlledVolumes.Count > 0) return instance.controlledVolumes[instance.controlledVolumes.Count - 1].volume;
+                if (Instance != null && Instance.controlledVolumes.Count > 0)
+                {
+                    return Instance.controlledVolumes[Instance.controlledVolumes.Count - 1].volume;
+                }
                 return 1;
             }
         }
+        /// <summary>
+        /// Add a volume override
+        /// </summary>
+        /// <param name="goSource">If null, it is a global override and will be auto-removed when global overrides are</param>
+        /// <param name="vol">The target volume level</param>
         public static void SetControlledVolume(GameObject goSource, float vol)
         {
             if (instance == null) return;
             instance.controlledVolumes.Add(new ControlledVolume()
             { source = goSource, volume = vol });
         }
+        /// <summary>
+        /// Remove override that was triggered by a source. Can be null to remove global
+        /// </summary>
+        /// <param name="goSource"></param>
         public static void RemoveControlledVolume(GameObject goSource)
         {
-            if (instance == null) return;
-            for (int i = instance.controlledVolumes.Count - 1; i >= 0; i--)
-            {
-                if (instance.controlledVolumes[i].source == goSource)
-                {
-                    instance.controlledVolumes.RemoveAt(i);
-                }
-            }
+            if (Instance == null) return;
+            Instance.controlledVolumes.RemoveAll(item => item.source == goSource);
         }
-        //private static List<QueuedSheet> fullQueue = new List<QueuedSheet>();
-        [Sirenix.OdinInspector.ReadOnly]
-        public List<QueuedSheet> queue = new List<QueuedSheet>();
+        /// <summary>
+        /// Requests to start playing a music sheet
+        /// </summary>
+        /// <param name="sheet"></param>
         public static void QueueMusicSheet(MusicSheet sheet)
         {
             if (Instance) Instance.InternalQueueMusicSheet(sheet);
         }
+        /// <summary>
+        /// begins the process of stopping the music sheet from playing based on the sheet's settings
+        /// </summary>
+        /// <param name="sheet"></param>
         public static void DequeueMusicSheet(MusicSheet sheet)
         {
             if (Instance) Instance.InternalDequeueMusicSheet(sheet);
         }
-
+        /// <summary>
+        /// Actual queue implemntation. It checks if we already have a similar sheet, if so re-set and use it instead
+        /// </summary>
+        /// <param name="_sheet"></param>
         protected void InternalQueueMusicSheet(MusicSheet _sheet)
         {
             if (queue == null) queue = new List<QueuedSheet>();
@@ -63,7 +88,6 @@
 
             if (_sheet.clips.Length == 0) return;
 
-            bool hasSheet = false;
             _sheet.Initialize();
             for (int i = 0; i < queue.Count; i++)
             {
@@ -71,7 +95,6 @@
                 //we already have this sheet
                 if (queue[i].sheet == _sheet)
                 {
-                    hasSheet = true;
                     queue[i].markedForDequeue = false;
                     queue[i].startTimer = 0;
                     // put it on top
@@ -81,22 +104,24 @@
                         queue[i] = queue[queue.Count - 1];
                         queue[queue.Count - 1] = tmp;
                     }
+
+                    return;
                 }
             }
 
-            if (hasSheet == false)
+            queue.Add(new QueuedSheet
             {
-                var qs = new QueuedSheet
-                {
-                    source = GetAvailableSource(),
-                    sheet = _sheet
-                };
-                queue.Add(qs);
-            }
+                source = GetAvailableSource(),
+                sheet = _sheet
+            });
         }
+        /// <summary>
+        /// Mark a sheet for removal
+        /// </summary>
+        /// <param name="_sheet"></param>
         protected void InternalDequeueMusicSheet(MusicSheet _sheet)
         {
-            if (queue == null) queue = new List<QueuedSheet>();
+            if (queue == null) return;
             for (int i = 0; i < queue.Count; i++)
             {
                 queue[i].startTimer = 0;
@@ -107,11 +132,12 @@
                 }
             }
         }
-
+        /// <summary>
+        /// Handle playing the music
+        /// </summary>
         private void Update()
         {
-            if (queue == null) queue = new List<QueuedSheet>();
-            if (queue.Count == 0) return;
+            if (queue == null || queue.Count == 0) return;
             bool topSheetPlayed = false;
             for (int i = queue.Count - 1; i >= 0; i--)
             {
@@ -170,7 +196,7 @@
                 }
             }
             // no available source, create one
-            GameObject newGO = new GameObject("Audio Source [" + sources.Count + "]");
+            GameObject newGO = new GameObject($"Audio Source [{sources.Count}]");
             newGO.transform.SetParent(transform);
             newGO.transform.localPosition = Vector3.zero;
             AudioSource newSource = newGO.AddComponent<AudioSource>();
